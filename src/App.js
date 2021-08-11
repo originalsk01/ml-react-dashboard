936c9b74ebb9e05b7ab5c50351461bb2b1d3fa9d
Sipho SK <sipssman@gmail.com>
2021-08-11 18:14:51 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontFamily: 'roboto'
+    }}>
+      <h1 style={{
+        color: '#6ec177',
+        margin: 0
+      }}
+      >Wah Gwan Soon come</h1>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders home page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const h1Element = screen.getByText(/Wah Gwan Soon come/i);
+  expect(h1Element).toBeInTheDocument();
 });
